530cf16cf876323bebe521e1c249644be726d829
gongph <gongph@foxmail.com>
2017-05-28 23:15:27 +0800

    new file

---
+++ src/app.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <button @click="toggle"> {{ msg }} </button>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        msg: 'hello, vue.js!'
+      }
+    },
+    methods: {
+      toggle () {
+        console.log('hello!')
+      }
+    }
+  }
+</script>
+
+<style>
+  p { color: red; }
+</style>
