9ee3891c291ebc5936d731dfde8f359725bf0ea3
gongph <gongph@foxmail.com>
2017-05-31 15:22:11 +0800

    del history mode

--- src/router.js
+++ src/router.js
@@ -1,17 +1,15 @@
 import VueRouter from 'vue-router'
 
-// import Foo from './components/foo.vue'
-// import Bar from './components/bar.vue'
-
 const Foo = resolve => require(['./components/foo.vue'], resolve)
 const Bar = resolve => require(['./components/bar.vue'], resolve)
 
 const routes = [
-  { path: '/foo', component: Foo },
-  { path: '/bar', component: Bar }
+  { path: '/foo/:id', name: 'foo', component: Foo },
+  { path: '/bar', name: 'bar', component: Bar }
 ]
 
 export const router = new VueRouter({
   routes,
-  mode: 'history'
+  base: __dirname,
+  // mode: 'history'
 })
