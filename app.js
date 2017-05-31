7d59e3d427d80644039e1ab7f4038a6e2c29a681
gongph <gongph@foxmail.com>
2017-05-31 13:50:16 +0800

    import vue-router

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import { router } from './src/router.js'
 import App from './src/app.vue'
 
+Vue.use(VueRouter)
+
 new Vue({
-  el: '#app',
+  router,
   render: h => {
     return h(App)
   }
-})
+}).$mount('#app')
+
